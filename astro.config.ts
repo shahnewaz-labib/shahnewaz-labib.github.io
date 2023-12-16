c65c082e7583f4eaa7ee40b1d52c59ee9cbffd6d
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2023-12-17 00:25:21 +0600

    went back to astropaper

---
+++ astro.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://shahnewaz-labib.github.io',
+  // base: '/shahnewaz-labib.github.io'
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  scopedStyleStrategy: "where",
+});
